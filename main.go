e5680467824e87afc7669ac36dcf154c54fc264b
HendricksK <kurvinhendricks@gmail.com>
2022-08-14 21:51:39 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type image struct {
+	RequestedTopic string   `json:"requestedTopic"`
+	Images         []string `json:"Images"`
+	Number         int      `json:"Number"`
+}
+
+var images = []image{
+	{
+		RequestedTopic: "the+room+memes",
+		Images: []string{
+			"https://preview.redd.it/0ndypduzlf641.jpg?width=640&crop=smart&auto=webp&s=5cf49a2deb07cfa5bf628e1dfb074b0452bb8041",
+			"https://preview.redd.it/q6ez8ub45i931.jpg?width=640&crop=smart&auto=webp&s=4b4859c1cc752ac3310496852b8fd19a77aa7fc7",
+		},
+		Number: 2,
+	},
+}
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	router.GET("/topics", getImageTopics)
+	// router.POST("/albums", postAlbums)
+	// router.GET("/albums/:id", getAlbumByID)
+	// router.GET("/albums/title/:title", getAlbumByTitle)
+	// router.GET("/albums/artist/:artist", getAlbumsByArtist)
+	return router
+}
+
+func main() {
+	router := setupRouter()
+	router.Run("localhost:8080")
+}
+
+func getImageTopics(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, images)
+}
